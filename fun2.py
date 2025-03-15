7d7bb1877b313ed75b536cecafdb6c9cabf44717
Manu1737 <manueljaby@gmail.com>
2025-03-15 18:18:55 +0100

    fun3

---
+++ fun2.py
@@ -0,0 +1,6 @@
+def saludo_personalizado(nombre, edad):
+    print(f"Hola, {nombre}. Tienes {edad} años.")
+
+saludo_personalizado("María", 25)
+saludo_personalizado("Juan", 30)
+saludo_personalizado("Lucía", 22)
