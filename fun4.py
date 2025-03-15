74689b516719b3e90f934229c5a763b70a749edd
Manu1737 <manueljaby@gmail.com>
2025-03-15 18:22:16 +0100

    fun4

---
+++ fun4.py
@@ -0,0 +1,10 @@
+def multiplicar(a, b):
+    return a * b
+
+resultado1 = multiplicar(3, 4)
+resultado2 = multiplicar(7, 2)
+resultado3 = multiplicar(5, 6)
+
+print(resultado1)
+print(resultado2)
+print(resultado3)
