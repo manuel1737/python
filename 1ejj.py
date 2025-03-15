035b6f3449865d3b2393db4b6e91305220724a28
Manu1737 <manueljaby@gmail.com>
2025-03-15 15:31:34 +0100

    py

---
+++ 1ejj.py
@@ -0,0 +1,9 @@
+numeros = [1, 2, 3, 4, 5]
+primer_elemento = numeros[0]
+ultimo_elemento = numeros[-1]
+
+frutas = ["manzana", "pera", "naranja"]
+frutas[1] = "uva"
+
+numeros = [10, 20, 30]
+numeros.extend([40, 50, 60])
